6528684b2931941674111e56bfbb5f53ca7bd952
PavloDereniuk <125708520+PavloDereniuk@users.noreply.github.com>
2023-11-15 02:42:44 +0200

    add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,8 @@ import { fetchImages } from 'api';
 import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import { Button } from './Button/Button';
-import { Loader } from './Loader/Loader';
+// import { Loader } from './Loader/Loader';
 
 export class App extends Component {
   state = {
